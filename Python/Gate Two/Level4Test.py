8fa1ec947aaafa921d539bb7b5c20f16b2eee187
Your Name <youremail@example.com>
2026-03-17 17:09:41 +0100

    initial commit

---
+++ Python/Gate Two/Level4Test.py
@@ -0,0 +1,12 @@
+import unittest
+from Level_4_function import TestCase:
+#import TestCase
+class Level_4_functionTestCase(self):
+    Level_4_function_addition_of_two_numbers(self):
+    Expected = 15
+    assert.Actual(a + b)
+
+
+
+
+
---
+++ Python/Gate Two/Level4function.py
@@ -0,0 +1,54 @@
+#def add_numbers(a,b):
+#    return a + b
+#
+#result=add_numbers(9,6)
+#print(result)
+#
+#def even_numbers(num):
+#    for number in num(1, num+1):
+#        if number % 2 == 0:
+#            return number
+#
+#result=even_numbers(6)
+#print(result)
+#
+#def square_of_numbers(b):
+#    return b*b
+#
+#result=square_of_numbers(6)
+#print(result)
+#
+#def convert_to_fahrenheit():
+#    fahrenheit=(celcius*9/5)+32):
+#        return fahrenheit
+#
+#result=convert_to_fahrenheit(100)
+#print(result)
+#
+#def check_for_prime_number(num):
+#    for nums in num:
+#        if nums % 2!=0:
+#            return nums
+#
+#result=check_for_prime_number(100)
+#print(result)
+#
+#def area_of_a_rectangle(l, b):
+#    return l*b
+#
+#result=area_of_a_rectangle(12, 4)
+#print(result)
+#
+#def simple_interest():
+#    price=50
+#    rate=0.05
+#    time=2
+#    interest=(price*rate*time/100)
+#    return interest
+#
+#result=simple_interest()
+#print(result)
+# 
+#
+#def multiplication_table():
+#
