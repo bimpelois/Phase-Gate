348c0e22705cf78bc6f8ef649a31df2db1cc8ccc
Your Name <youremail@example.com>
2026-03-17 17:07:30 +0100

    initial commit

---
+++ Java/Gate Two/Level4Test.java
@@ -0,0 +1,11 @@
+import org.junit.junpiter.test.Api;
+import static.org.junit.junpiter.api.assertion.assertEquals;
+import static.org.junit.junpiter.api.assertion.assertTrue;
+import static.org.junit.junpiter.api.assertion.assertFalse;
+
+    public class TaskFourFunction{
+
+    @Test
+public void AdditionOfTwoNumber;
+      
+
--- Java/Gate Two/TaskFive.java
+++ Java/Gate Two/TaskFive.java
@@ -1,12 +1,11 @@
 public class TaskFive{
     public static void main(String[] args){
     int number=6;
-    int factorial=0;
-    for(int count=1; count<=6; count++){
-        for(int index=1; index<=count; index++){
-            factorial*=index;
-        System.out.println(factorial);       
+    int factorial=1;
+    for(int count=1; count<=number; count++){
+        factorial*=count;
+               }
+System.out.println("factorial = " + factorial);
 }
-}
-}
+
 }
