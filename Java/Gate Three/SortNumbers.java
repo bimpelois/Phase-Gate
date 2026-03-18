5cbff31de29ab1e08ee6982f47c1daea2deacd22
Your Name <youremail@example.com>
2026-03-18 17:16:11 +0100

    initial commit

---
+++ Java/Gate Three/SortNumbers.java
@@ -0,0 +1,16 @@
+public class SortNumbers{
+    public static void main(String[] args){
+
+    int []numbers={
+                  {45.2, 90.28, 77.1};
+                  {3.52, 9.86, 11.77};
+                  {77, 10, 22.33, 12};
+                                      };
+
+for(int index=0; index<=numbers;index++){
+    for(int count=0; count<=numbers.length; count++){
+}
+System.out.println(number[0][1]);
+}
+}
+}
---
+++ Java/Gate Three/SortString.java
@@ -0,0 +1,12 @@
+public class SortString{
+    public static void main(String[] args){
+
+String letter = "AaaBBc";
+              = "EEEEnnnneee";
+              = "ooooooYYOO";
+for(int count=0; count<=letter.length(); count++){
+            
+    System.out.println(letter.charAt(count));
+}
+}
+}
