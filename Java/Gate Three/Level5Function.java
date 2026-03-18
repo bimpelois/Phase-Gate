5c2bed190cda68f6f59aae3483661694294f9112
Your Name <youremail@example.com>
2026-03-18 13:19:28 +0100

    initial commit

---
+++ Java/Gate Three/Level5Function.java
@@ -0,0 +1,36 @@
+import java.util.Arrays
+public class Level5Function{
+    public static void main(String[]args){
+     
+int []numbers = {10,11,12};
+
+int []newArray=numbers{5};
+for(int count=0; count<=newArray.length(); count++){
+}
+
+}
+System.out.println(newArray);
+}
+}
+
+int []numbers = {10,11,12,13,14};
+
+
+for(int count=0; count<=number.length(-3); count++){
+}
+
+}
+System.out.println(count);
+}
+}
+
+int []numbers = {};
+int []newArrays=4:
+
+for(int count=0; count<=number.length; count++){
+}
+
+}
+System.out.println(count);
+}
+}
