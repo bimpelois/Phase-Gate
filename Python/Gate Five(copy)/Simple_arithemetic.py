397303c1f88aba417d4da9e3409203bd67ebc0f1
Your Name <youremail@example.com>
2026-03-23 15:52:22 +0100

    initial commit

---
+++ Python/Gate Five(copy)/Simple_arithemetic.py
@@ -0,0 +1,11 @@
+number_subtraction = input(int("enter 10 questions: "))
+num1 = random randint(1,10)
+num2 = random randint(0,9)
+for number in range(num1,num2):
+    if(answer=num1+num2):
+        print(number)
+    else if(answer=num1-num2):
+        print(number)
+    else(answer=num1/num2):
+        print(number)
+
