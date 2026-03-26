15defa523c6b1fa8010dee81fdf1e039338e446b
Your Name <youremail@example.com>
2026-03-26 12:28:38 +0100

    initial commit

---
+++ Java/GateEight/PhoneBookApp.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+public class PhoneBookApp{
+public static void main(String[] args){
+Scanner input = new Scanner(System.in);
+
+public void contact menu() {
+            System.out.println("\n1. Add Contact needs :");
+            System.out.println("2. Remove Contact");
+            System.out.println("3. Find Contact by Phone Number");
+            System.out.println("4. Find Contact by First Name");
+            System.out.println("5. Find Contact by Last Name");
+            System.out.println("6. Edit Contact");
+            System.out.print("Enter choice: ");
+
+            int choice = input.nextInt();
+
+}
+public void Add contact needs(){
+            System.out.println("Enter First Name: ");
+            String firstName = input.nextLine();
+            System.out.println("Enter Last Name: ");
+            String lastName = input.nextLine();
+            System.out.println("Enter Phone Number: ");
+            String phoneNumber = input.nextLine();
+}
+public void Remove contact(){
+            System.out.println("Enter Phone Number to delete: ");
+            String phoneNumber = input.nextLine();
+            Contact.RemoveIf(contact-> Contact.getPhoneNumber().equals(phoneNumber));
+}
+
+System.out.println("First Name: " + firstName);
+System.out.println("Last Name: " + lastName);
+System.out.println("Phone Number: " + phoneNumber);
+System.out.println("contact added successfully");
+
+}
+}
